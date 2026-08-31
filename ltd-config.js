c31b4958553a035ddaa6a05a96dc7782de56c96c
ltdrobot <ltdrobot@localizethedocs.org>
2026-08-31 17:28:47 +0000

    Deploying to pages from @ localizethedocs/pytest-docs-l10n@ae047f60f5cad5b71a5c07a116240d00f448b111 🚀

---
+++ ltd-config.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["latest",  "Development"],
+    ["stable",  "Stable Release"],
+    ["9",       "Release 9.x"],
+    ["8",       "Release 8.x"],
+    ["7",       "Release 7.x"],
+    ["6",       "Release 6.x"],
+    ["5",       "Release 5.x"],
+    ["4",       "Release 4.x"],
+    ["3",       "Release 3.x"],
+    ["2",       "Release 2.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/pytest-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/pytest-docs-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/pytest-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/pytest-docs-l10n"],
+  ]
+};
